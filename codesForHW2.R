5a84b194eebdab19872bfef8f3c04941701b980f
PatrickA20 <patrick20anderson@gmail.com>
2025-01-09 12:09:47 -0500

    HW

--- codesForHW2.R
+++ codesForHW2.R
@@ -14,7 +14,7 @@ library(dplyr)
 arrests=rio::import(linkMass,which = 1)
 codes_data=rio::import(linkMass,which = 2)
 
-names(arrests2)
+names(codes_data)
 # Load necessary library
 library(readxl)
 library(dplyr)
@@ -45,13 +45,13 @@ library(dplyr)
 # Update the Race column using case_when
 merged_data <- merged_data %>%
   mutate(Race = case_when(
-    Race == "B" ~ "Black / African American",
+    Race == "B" ~ "Black",
     Race == "W" ~ "White",
     Race == "H" ~ "Hispanic",
-    Race == "O" ~ "Asian or Pacific Islander",
-    Race == "I" ~ "American Indian or Alaskan Native",
+    Race == "O" ~ "Asian",
+    Race == "I" ~ "American Indian",
     Race == "U" ~ "Unknown",
-    Race == "J" ~ "Middle Eastern or East Indian (South Asia)",
+    Race == "J" ~ "Middle Eastern",
     Race == "N" ~ "Not Applicable",
     TRUE ~ Race  # Retain original value if no match
   ))
@@ -67,12 +67,10 @@ arrests <- merged_data %>%
 
 head(arrests)
 names(arrests)
-arrests_new <- arrests |>
-  select(`Arrest Offense by UCR Code`, Race)
 
 # see data types ----------------------------------------------------------
 
-str(arrests_new)
+str(arrests)
 
 # contingency table ----------------------------------------------------------
 
@@ -90,33 +88,42 @@ contingency_mgCol=100*prop.table(contingency,margin = 2)
 #adding marginal
 contingencyDF$pctCol=round(as.data.frame(contingency_mgCol)[,3],1)
 
+
+# Filter the data for specific races
+contingencyDF <- contingencyDF %>%
+  filter(Race %in% c("Asian", 
+                     "Black", 
+                     "Hispanic", 
+                     "White"))
+
+# Filter for 8 specific crimes
+contingencyDF <- contingencyDF %>%
+  filter(UCR_Code %in% c("Rape", "Robbery", "Aggravated Assault", 
+                         "DUI", "Weapon Law Violations", 
+                         "Stolen Property Offenses", "Kidnapping", 
+                         "All Other Offenses"))
+
+
 contingencyDF <- contingencyDF |>
   select(UCR_Code,Race, counts, pctCol)
 # result for ggplot:
 head(contingencyDF,20)
 
+contingencyDF <- contingencyDF %>%
+  group_by(Race) %>%
+  mutate(UCR_Code = fct_reorder(UCR_Code, pctCol, .desc = TRUE)) %>%
+  ungroup()
+
 
 # deliverable 2 ----------------------------------------------------------
 library(ggplot2)
-#bad idea
-ggplot(data=contingencyDF,
-       aes(x=Race,y=pctCol,fill=UCR_Code)) + 
-  geom_bar(stat = "identity", position = 'fill') 
 
-#new base
-basePoints = ggplot(contingencyDF, aes(x=Race,y=reorder(UCR_Code,pctCol))) 
-# plot value as point, size by value of percent
-tablePlot = basePoints + geom_point(aes(size = pctCol)) 
-# add value of Percent as label
-tablePlot = tablePlot + geom_text(aes(label = pctCol),
-                                  nudge_x = 0.3,
-                                  size=3)
 
 # as usual for barplot (less info than base1)
 base4 = ggplot(contingencyDF, aes(x = UCR_Code, y = pctCol ) ) 
 
 #the bars
-bars4  = base4 + geom_bar( stat = "identity" ) + theme_minimal()
+bars4  = base4 + geom_bar( stat = "identity" , width = 0.7, fill = "blue") + theme_minimal()
 
 # bar per day time with 'facet'
 bars4 = bars4 + facet_grid(~ Race) 
@@ -129,7 +136,7 @@ baseRE  = ggplot(contingencyDF,
                  aes(x = reorder(UCR_Code, pctCol), #here
                      y = pctCol ) ) + theme_minimal()
 
-barsRE = baseRE + geom_bar( stat = "identity" ) 
+barsRE = baseRE + geom_bar( stat = "identity", width = 0.7, fill = "lightblue") 
 barsREFacet = barsRE + facet_grid( ~ Race) 
 barsREFacet= barsREFacet + coord_flip() 
 
@@ -142,19 +149,24 @@ barsREFacet + theme(axis.text.y = element_text(size=7,angle = 20)) +
                              pctCol,"")),
             nudge_y = 10)
 
-del1_hw2 <- barsREFacet + theme(axis.text.y = element_text(size=7,angle = 20)) + 
-  geom_text(aes(label=ifelse(pctCol>5,# condition to annotate
-                             pctCol,"")),
-            nudge_y = 10) +
+barsREFacet <- barsREFacet + 
+  theme(
+    strip.text = element_text(size = 5, angle = 5, face = "bold"), # Increase race label size
+  )
+
+del1_hw2 <- barsREFacet + theme(axis.text.y = element_text(size=7,angle = 20), 
+                                axis.text.x = element_text(size = 5)) + 
+  geom_text(aes(label = ifelse(pctCol > 5, pctCol, "")),
+            nudge_y = 8, size = 1.75, fontface = "bold", vjust = 0.5) + 
+
   labs(title= "Percentage of Arrests by UCR Code per Race",
        subtitle = "Arrest information from January 2019 to March 2020.",
        caption = "Massachusetts Arrest Data - Field Services and Investigative Divisions",
-       x="",y="%")
+       x="",y="Percentages")
 
 del1_hw2
 
 
 # save del1Draft ----------------------------------------------------------
 saveRDS(del1_hw2, file = "del1_hw2.rds")
-
 
